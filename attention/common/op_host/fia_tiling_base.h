9ac275b5300b7331857cdfc64fe25fe663be7cdc
abaabc <zhaozhiyong15@hisilicon.com>
2025-11-11 22:14:23 +0800

    FIA set schedulemode
    Co-authored-by: zhaozhiyong<zhaozhiyong15@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !304 merge batchmode2 into master
    
    FIA set schedulemode
    
    Created-by: abaabc
    Commit-by: zhaozhiyong
    Merged-by: turing_project1
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #123-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/ops-transformer!304

--- attention/common/op_host/fia_tiling_base.h
+++ attention/common/op_host/fia_tiling_base.h
@@ -33,6 +33,12 @@ namespace optiling {
 
 class TilingInfo {};
 
+enum class ScheduleMode : uint32_t {
+    NORMAL_MODE = 0,
+    BATCH_MODE = 1,
+    SYNC_MODE = 2
+};
+
 class FiaTilingBase {
 public:
     FiaTilingBase() = default;
@@ -96,6 +102,11 @@ protected:
         context_->GetRawTilingData()->SetDataSize(tilingData.GetDataSize());
 
         return ge::GRAPH_SUCCESS;
+    }
+
+    [[nodiscard]] ge::graphStatus SetScheduleMode(ScheduleMode scheduleMode) const
+    {
+        return context_->SetScheduleMode(static_cast<uint32_t>(scheduleMode));
     }
 
     template <typename T>
