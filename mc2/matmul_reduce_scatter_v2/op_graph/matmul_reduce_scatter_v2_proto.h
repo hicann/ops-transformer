3b26e17d9762753acedce3246a0183127450e3e8
Kiana1216 <wangkechen3@huawei.com>
2026-07-28 11:45:40 +0800

    MatmulReduceScatterV2支持mxfp4图模式
    Co-authored-by: wangkechen<wangkechen3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !9240 merge mmrs_mxfp4_graph into master
    
    MatmulReduceScatterV2支持mxfp4图模式
    
    Created-by: Kiana1216
    Commit-by: wangkechen
    Merged-by: cann-robot
    Description: ## 描述
    MatmulReduceScatterV2支持mxfp4图模式，在图原型增加x1和x2支持float4_e2m1数据类型
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-transformer/issues/3963
    
    ## 测试
    本地测试和rdv
    ![精度pass截图.PNG](https://raw.gitcode.com/user-images/assets/7673863/592dc550-9ad8-4d48-91d1-1ce6b2c8e04b/精度pass截图.PNG '精度pass截图.PNG')
    
    ## 文档更新
    不涉及，图模式的资料在idp平台，当前的资料已支持mxfp4场景
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 Bug修复
    - [x] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    See merge request: cann/ops-transformer!9240

--- mc2/matmul_reduce_scatter_v2/op_graph/matmul_reduce_scatter_v2_proto.h
+++ mc2/matmul_reduce_scatter_v2/op_graph/matmul_reduce_scatter_v2_proto.h
@@ -23,24 +23,27 @@ namespace ge {
 * @brief Fusion op of matmul and reduce scatter v2.
 * @par Inputs:
 * six inputs, including:
-* @li x1: A matrix tensor. The type supports float16, bfloat16, hifloat8, float8_e5m2, float8_e4m3fn.The format supports
-ND. The x1 only supports two dimensions in current version, for example (M, K). The x1 doesn't support transposed.
-* @li x2: A matrix tensor. The type supports float16, bfloat16, hifloat8, float8_e5m2, float8_e4m3fn.The x2 only
-supports two dimensions in current version, for example (K, N). The x2 supports transposed and non-transposed. The K
-value in x2 should be same as the K value in x1 when x2 is non-transposed, and the K value should be in range [256,
-65535).
-* @li bias: A matrix tensor. The format supports ND. If x1 type is float8_e4m3fn, float8_e5m2, hifloat8, then the bias
-type supports float32. \n If x1 type is float16, bfloat16, then the bias type supports float16, bfloat16 and the current
-version doesn't supports the scenario where bias is not 0. \n The bias only supports one dimension in current version,
-for expample (N,). \n The bias only supports nullptr in the per_block scenario. \n
+* @li x1: A matrix tensor. The type supports float16, bfloat16, hifloat8, float8_e5m2, float8_e4m3fn, float4_e2m1. The
+format supports ND. The x1 only supports two dimensions in current version, for example (M, K). The x1 doesn't support
+transposed.
+* @li x2: A matrix tensor. The type supports float16, bfloat16, hifloat8, float8_e5m2, float8_e4m3fn, float4_e2m1. The
+x2 only supports two dimensions in current version, for example (K, N). The x2 supports transposed and non-transposed.
+The K value in x2 should be same as the K value in x1 when x2 is non-transposed, and the K value should be in range
+[256, 65535).
+* @li bias: A matrix tensor. The format supports ND. If x1 type is float8_e4m3fn, float8_e5m2, hifloat8, float4_e2m1,
+then the bias type supports float32. \n If x1 type is float16, bfloat16, then the bias type supports float16, bfloat16
+and the current version doesn't supports the scenario where bias is not 0. \n The bias only supports one dimension in
+current version, for expample (N,). \n The bias only supports nullptr in the per_block scenario. \n
 * @li x1_scale: A matrix tensor. The type supports float32, float_e8m0. The format supports ND. \n The x1_scale only
 supports nullptr when x1's type is float16 or bfloat16. \n The x1_scale only supports one dimension and only one element
 in the per_tensor scenario, for expample (1,). \n The x1_scale only supports two dimensions in the per_block scenario,
-for expample (ceildiv(M, 128), ceildiv(K, 128)). \n
+for expample (ceildiv(M, 128), ceildiv(K, 128)). \n The x1_scale only supports three dimensions in the mxfp scenario,
+for expample (M, ceildiv(M, 64), 2). \n
 * @li x2_scale: A matrix tensor. The type supports float32, float_e8m0. The format supports ND. \n The x2_scale only
 supports nullptr when x1's type is float16 or bfloat16. \n The x2_scale only supports one dimension and only one element
 in the per_tensor scenario, for expample (1,). \n The x2_scale only supports two dimensions in the per_block scenario,
-for expample (ceildiv(K, 128), ceildiv(N, 128)). \n
+for expample (ceildiv(K, 128), ceildiv(N, 128)). \n The x2_scale only supports three dimensions in the mxfp scenario,
+for expample (M, ceildiv(M, 64), 2). \n
 * @li quant_scale: A matrix tensor. The type supports float32. The format supports ND. The quant_scale only supports one
 dimension and only one element, for example (1,). The quant_scale only supports nullptr in current version. \n
 *
@@ -60,24 +63,24 @@ version.
 * @li block_size: An int. Number of values in the M, N directions in y correspond to one dequantization coefficient.
 Default: "0". The block_size only supports 0 in current version.
 * @li group_size: An int. Number of values in the M, N, K directions in x1/x2 correspond to one dequantization
-coefficient. \n Default: "0". Reserved attribute. The block_size only supports 549764202624 in the per_block scenario.
-The block_size only supports 0 in the other scenario. \n
+coefficient. \n Default: "0". Reserved attribute. The group_size only supports 549764202624 in the per_block scenario.
+The group_size only supports 4295032864 in the mxfp scenario. The group_size only supports 0 in the other scenario. \n
 * @li is_amax_out: A bool. whether the output of amax_out is supported . Default: "false". If True, supports output
 amax_out.
 * @li y_dtype: An int. Default: "0". The y_dtype only supports 0(float32)/1(float16)/27(bfloat16) in current version. \n
-* @li comm_mode: A string. communication Mode. Default: "". The comm_mode only supports "aicpu" or "aiv" in current
-version. \n
+* @li comm_mode: A string. communication Mode. Default: "ai_cpu". The comm_mode only supports "ai_cpu" or "aiv" in
+current version. \n
 *
 * @par Outputs:
 * @li y: A matrix tensor. If x1 type is float16, bfloat16, The type is same as x1.
-  If x1 type is float8_e4m3fn, float8_e5m2, hifloat8, the output type supports float16, bfloat16, float32. The format
-supports ND. The y is two dimensions, for example (M/rank_size, N).
+  If x1 type is float8_e4m3fn, float8_e5m2, hifloat8, float4_e2m1, the output type supports float16, bfloat16, float32.
+The format supports ND. The y is two dimensions, for example (M/rank_size, N).
 * @li amax_out: A matrix tensor. The type supports float32. The format supports ND. The amax_out is 1 dimension and only
 one element, for example (1,). The amax_out only supports nullptr in current version. \n
 */
 REG_OP(MatmulReduceScatterV2)
-    .INPUT(x1, TensorType({DT_FLOAT16, DT_BF16, DT_FLOAT8_E4M3FN, DT_FLOAT8_E5M2, DT_HIFLOAT8}))
-    .INPUT(x2, TensorType({DT_FLOAT16, DT_BF16, DT_FLOAT8_E4M3FN, DT_FLOAT8_E5M2, DT_HIFLOAT8}))
+    .INPUT(x1, TensorType({DT_FLOAT16, DT_BF16, DT_FLOAT8_E4M3FN, DT_FLOAT8_E5M2, DT_HIFLOAT8, DT_FLOAT4_E2M1}))
+    .INPUT(x2, TensorType({DT_FLOAT16, DT_BF16, DT_FLOAT8_E4M3FN, DT_FLOAT8_E5M2, DT_HIFLOAT8, DT_FLOAT4_E2M1}))
     .OPTIONAL_INPUT(bias, TensorType({DT_FLOAT16, DT_BF16, DT_FLOAT}))
     .OPTIONAL_INPUT(x1_scale, TensorType({DT_FLOAT, DT_FLOAT_E8M0}))
     .OPTIONAL_INPUT(x2_scale, TensorType({DT_FLOAT, DT_FLOAT_E8M0}))
