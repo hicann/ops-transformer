18ae8ab2f46f700f7b1a0918ea193454154bf4c7
weihao18 <weihao16@h-partners.com>
2026-09-10 20:19:52 +0800

    fix: 同步ops-tensor commitID到最新commit修复allto_all_matmul编译报错
    Co-authored-by: weihao18<weihao16@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !11642 merge fix-issue-5158 into master
    
    fix: 同步ops-tensor commitID到最新commit修复allto_all_matmul编译报错
    
    Created-by: weihao18
    Commit-by: weihao18
    Merged-by: cann-robot
    Description: ## 描述
    fix: 同步ops-tensor commitID到最新commit修复allto_all_matmul编译报错
    
    ## 关联的Issue
    https://gitcode.com/cann/ops-transformer/issues/5158
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] 🐛 Bug修复
    - [ ] ✨ 新特性
    - [ ] ⚡ 性能优化
    - [ ] ♻️ 重构
    - [ ] 🧪 测试
    - [ ] 📦 构建/CI
    - [ ] 🔧 配置变更
    - [ ] 📝 文档更新
    - [ ] ⬆️ 依赖升级
    - [ ] 🔒 安全修复
    - [ ] 🧹 代码清理
    - [ ] ❓ 其他，请描述：
    
    
    See merge request: cann/ops-transformer!11642

--- cmake/third_party/ops-tensor.cmake
+++ cmake/third_party/ops-tensor.cmake
@@ -7,7 +7,7 @@
 # INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
 # See LICENSE in the root of the software repository for the full text of the License.
 # ----------------------------------------------------------------------------
-set(OPTENSOR_TAG_ID 781745c86312b478009742851b6cfc1967da8dda)
+set(OPTENSOR_TAG_ID 43d554d6913d5677e31255fc51b0238d9fdaa7cd)
 
 if(EXISTS "${PROJECT_SOURCE_DIR}/../ops-tensor")
   get_filename_component(OPTENSOR_SOURCE_PATH
